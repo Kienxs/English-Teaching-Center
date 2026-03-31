2fe190c1a456db61b9da84f14bb95755e7782ab5
kienxs <kienxs292005@gmail.com>
2026-04-01 00:45:17 +0700

    prevent key exposure

--- src/main/java/com/example/English/teaching/center/config/CloudinaryConfig.java
+++ src/main/java/com/example/English/teaching/center/config/CloudinaryConfig.java
@@ -5,17 +5,28 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.cloudinary.Cloudinary;
 
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dypxfew4x");
-        config.put("api_key", "568249469316255");
-        config.put("api_secret", "Q9_5PBxPoRXUDgX0KNW5nihgAdo");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 }
